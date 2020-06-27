c74f01a0a5547a49688427ccf0cf702d364eccbe
Jaakko Heusala <jheusala@iki.fi>
2020-06-27 22:43:21 +0300

    Updated test script to use 10% puts

--- scripts/multi-curl.sh
+++ scripts/multi-curl.sh
@@ -3,7 +3,8 @@
 (
   for i in {1..1000}; do
 
-    if test "$RANDOM" -gt 16383; then
+    #if test "$RANDOM" -lt 16383; then 50%
+    if test "$RANDOM" -lt 3276; then # 10%
       ( echo PUT "$( curl -s -X 'PUT' -d '"'"$RANDOM"'"' http://localhost:8000/hello )" )& < /dev/null
     else
       ( echo GET "$( curl -s http://localhost:8000/hello )" )& < /dev/null
