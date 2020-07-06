8c97b3cd1f9f8b103a7af8df67e6532f9cd47daa
Jaakko Heusala <jheusala@iki.fi>
2020-07-06 23:19:28 +0300

    Added default for REST_PATH

--- lib/SimpleREST/index.php
+++ lib/SimpleREST/index.php
@@ -10,6 +10,10 @@ if(!defined('REST_PHP')) {
 	define('REST_PHP', TRUE);
 }
 
+if(!defined('REST_PATH')) {
+	define('REST_PATH', '');
+}
+
 require_once( dirname(__FILE__) . '/Assert.class.php' );
 require_once( dirname(__FILE__) . '/Random.class.php' );
 require_once( dirname(__FILE__) . '/JSON.class.php' );
