885a1feb222e3794edc793406cb6c484c42bcd28
Jaakko Heusala <jheusala@iki.fi>
2020-07-09 14:55:53 +0300

    Added new Decimal operators

--- lib/SimpleREST/Decimal/Decimal.class.php
+++ lib/SimpleREST/Decimal/Decimal.class.php
@@ -35,7 +35,98 @@ class Decimal implements JsonSerializable {
 
     Assert::extensionLoaded("bcmath");
 
-    $this->_value = "" . $value;
+    $this->_value = "" . trim($value);
+
+  }
+
+  /**
+   * @return bool
+   */
+  public function isNegative () {
+
+    return substr($this->_value, 0, 1) === '-';
+
+  }
+
+  /**
+   * @returns bool TRUE if this value has no digits after the dot,
+   */
+  public function isInteger () {
+
+    return !self::_hasDecimalPoint( self::_removeLeadingZerosAfterDecimalPoint($this->_value) );
+
+  }
+
+  /**
+   * @param Decimal $value
+   * @throws Exception
+   * @return bool TRUE if this value is greater than $value
+   */
+  public function isGreaterThan (Decimal $value) {
+
+    return self::isGreater($this->_value, "" . $value);
+
+  }
+
+  /**
+   * @param Decimal $value
+   * @throws Exception
+   * @return bool TRUE if this value is greater than $value
+   */
+  public function isLowerThan (Decimal $value) {
+
+    return self::isLower($this->_value, "" . $value);
+
+  }
+  /**
+   * @param Decimal $value
+   * @throws Exception
+   * @return bool TRUE if this value is greater than $value
+   */
+  public function isGreaterThanOrEqual (Decimal $value) {
+
+    return self::isGreaterOrEqual($this->_value, "" . $value);
+
+  }
+
+  /**
+   * @param Decimal $value
+   * @throws Exception
+   * @return bool TRUE if this value is greater than $value
+   */
+  public function isLowerThanOrEqual (Decimal $value) {
+
+    return self::isLowerOrEqual($this->_value, "" . $value);
+
+  }
+
+  /**
+   * @param string $value
+   * @return bool
+   */
+  private static function _hasDecimalPoint (string $value) {
+    return strpos($value, '.') !== FALSE;
+  }
+
+  /**
+   * @param string $value
+   * @return string
+   */
+  private static function _removeLeadingZerosAfterDecimalPoint (string $value) {
+
+    if (!self::_hasDecimalPoint($value)) return $value;
+
+    return rtrim(rtrim($value, '0'), '.');
+
+  }
+
+  /**
+   * @param string $value
+   * @return string
+   */
+  private static function _normalize (string $value) {
+
+    return self::_removeLeadingZerosAfterDecimalPoint($value);
 
   }
 
@@ -59,8 +150,11 @@ class Decimal implements JsonSerializable {
    * @param array $values
    * @return Decimal
    * @throws Exception if less than one argument
+   * @throws Exception if bcmath extension is not loaded
    */
   static public function sum (...$values) {
+
+    Assert::extensionLoaded("bcmath");
 
     if (count($values) < 0) {
       throw new Exception('Must have at least one argument!');
@@ -80,8 +174,11 @@ class Decimal implements JsonSerializable {
    * @param array $values
    * @return Decimal
    * @throws Exception if less than two argument
+   * @throws Exception if bcmath extension is not loaded
    */
   static public function sub (...$values) {
+
+    Assert::extensionLoaded("bcmath");
 
     if (count($values) <= 1) {
       throw new Exception('Must have at least two arguments!');
@@ -100,8 +197,11 @@ class Decimal implements JsonSerializable {
    * @return Decimal
    * @throws Exception if one or less arguments
    * @throws Exception if divided by zero
+   * @throws Exception if bcmath extension is not loaded
    */
   static public function div (...$values) {
+
+    Assert::extensionLoaded("bcmath");
 
     if (count($values) <= 1) {
       throw new Exception('Must have at least two arguments!');
@@ -109,7 +209,7 @@ class Decimal implements JsonSerializable {
 
     return new Decimal( reduce($values, function($a, $b) {
 
-      $value = \bcdiv("" . $a, "" . $b, self::INTERNAL_SCALE);
+      $value = bcdiv("" . $a, "" . $b, self::INTERNAL_SCALE);
 
       if ($value === NULL) {
         throw new Exception('Divided by zero!');
@@ -127,8 +227,11 @@ class Decimal implements JsonSerializable {
    * @param array $values
    * @return Decimal
    * @throws Exception if one or less arguments
+   * @throws Exception if bcmath extension is not loaded
    */
   static public function mul (...$values) {
+
+    Assert::extensionLoaded("bcmath");
 
     if (count($values) <= 1) {
       throw new Exception('Must have at least two arguments!');
@@ -148,9 +251,61 @@ class Decimal implements JsonSerializable {
    * @param mixed $a
    * @param mixed $b
    * @return int
+   * @throws Exception if bcmath extension is not loaded
    */
   static public function compare ($a, $b) {
+
+    Assert::extensionLoaded("bcmath");
+
     return (int) bccomp("" . $a, "" . $b, self::INTERNAL_SCALE);
+
+  }
+
+  /**
+   * @param mixed $a
+   * @param mixed $b
+   * @return bool TRUE if $a is greater than $b
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function isGreater ($a, $b) {
+
+    return self::compare($a, $b) === 1;
+
+  }
+
+  /**
+   * @param mixed $a
+   * @param mixed $b
+   * @return bool TRUE if $a is greater than $b
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function isLower ($a, $b) {
+
+    return self::compare($a, $b) === -1;
+
+  }
+  /**
+   * @param mixed $a
+   * @param mixed $b
+   * @return bool TRUE if $a is greater than $b
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function isGreaterOrEqual ($a, $b) {
+
+    return self::compare($a, $b) >= 0;
+
+  }
+
+  /**
+   * @param mixed $a
+   * @param mixed $b
+   * @return bool TRUE if $a is greater than $b
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function isLowerOrEqual ($a, $b) {
+
+    return self::compare($a, $b) <= 0;
+
   }
 
   /**
@@ -158,8 +313,11 @@ class Decimal implements JsonSerializable {
    *
    * @param array $values
    * @return bool if every value in $value is equal or $values is empty
+   * @throws Exception if bcmath extension is not loaded
    */
   static public function isEqual (...$values) {
+
+    Assert::extensionLoaded("bcmath");
 
     if (count($values) === 0) return true;
 
@@ -175,13 +333,121 @@ class Decimal implements JsonSerializable {
 
   /**
    * @param Decimal $value
+   * @return Decimal
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function floor (Decimal $value) {
+
+    Assert::extensionLoaded("bcmath");
+
+    if ($value->isInteger()) return new Decimal( self::_normalize('' . $value) );
+
+    if ( $value->isNegative() ) {
+      return new Decimal( self::_normalize(bcsub($value, 1, 0)) );
+    }
+
+    return new Decimal( self::_normalize(bcsub($value, 0, 0)) );
+
+  }
+
+  /**
+   * @param Decimal $value
+   * @return Decimal
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function ceil (Decimal $value) {
+
+    Assert::extensionLoaded("bcmath");
+
+    if ($value->isInteger()) return new Decimal( self::_normalize('' . $value) );
+
+    if (!$value->isNegative()) {
+      return new Decimal( self::_normalize(bcadd($value, 1, 0)) );
+    }
+
+    return new Decimal( self::_normalize(bcsub($value, 0, 0)) );
+
+  }
+
+  /**
+   * @param Decimal $value
+   * @return Decimal
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function abs (Decimal $value) {
+
+    if ( !$value->isNegative() ) return $value;
+
+    return new Decimal( ltrim( '' . $value, '-') );
+
+  }
+
+  /**
+   * @param Decimal $value
+   * @return Decimal
+   * @throws Exception if bcmath extension is not loaded
+   */
+  static public function round (Decimal $value) {
+
+    Assert::extensionLoaded("bcmath");
+
+    if ($value->isInteger()) return new Decimal( self::_normalize('' . $value) );
+
+    if ($value->isNegative()) {
+      $decimals = self::sub($value, self::ceil($value));
+    } else {
+      $decimals = self::sub($value, self::floor($value));
+    }
+
+    if ($value->isNegative()) {
+
+      if ( $decimals->isGreaterThanOrEqual( new Decimal('-0.5') ) ) {
+        return self::ceil($value);
+      }
+
+      return self::floor($value);
+
+    }
+
+    if ( $decimals->isGreaterThanOrEqual( new Decimal('0.5') ) ) {
+      return self::ceil($value);
+    }
+
+    return self::floor($value);
+
+  }
+
+  /**
+   * @param Decimal $value
    * @param int $scale
    * @return string
    * @throws Exception if bcmath extension is not loaded
    */
   static public function format (Decimal $value, int $scale) {
 
+    Assert::extensionLoaded("bcmath");
+
     return bcadd("".$value, 0, $scale);
+
+  }
+
+  /**
+   * @return array
+   */
+  public function __debugInfo () {
+    return [
+      '_value' => $this->_value
+    ];
+  }
+
+  /**
+   * @param array $properties
+   * @return Decimal
+   * @throws Exception if extension bcmath is not loaded
+   */
+  public function __set_state (array $properties) {
+
+    return new Decimal( $properties['_value'] );
 
   }
 
--- tests/SimpleREST/Decimal/DecimalTest.class.php
+++ tests/SimpleREST/Decimal/DecimalTest.class.php
@@ -34,6 +34,136 @@ final class DecimalTest extends TestCase {
   /**
    *
    */
+  public function testCanDetectPositiveIntegers () {
+
+    $this->assertSame(
+      false,
+      (new Decimal('1.23'))->isInteger(),
+      '1.23'
+    );
+
+    $this->assertSame(
+      false,
+      (new Decimal('1.2'))->isInteger(),
+      '1.2'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('1'))->isInteger(),
+      '1'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('1.0'))->isInteger(),
+      '1.0'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('0'))->isInteger(),
+      '0'
+    );
+
+  }
+
+  /**
+   *
+   */
+  public function testCanDetectNegativeIntegers () {
+
+    $this->assertSame(
+      false,
+      (new Decimal('-1.23'))->isInteger(),
+      '-1.23'
+    );
+
+    $this->assertSame(
+      false,
+      (new Decimal('-1.2'))->isInteger(),
+      '-1.2'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('-1'))->isInteger(),
+      '-1'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('-1.0'))->isInteger(),
+      '-1.0'
+    );
+
+  }
+
+  /**
+   *
+   */
+  public function testCanDetectPositiveDecimals () {
+
+    $this->assertSame(
+      false,
+      (new Decimal('1.23'))->isNegative()
+    );
+
+    $this->assertSame(
+      false,
+      (new Decimal('1.2'))->isNegative()
+    );
+
+    $this->assertSame(
+      false,
+      (new Decimal('1'))->isNegative()
+    );
+
+    $this->assertSame(
+      false,
+      (new Decimal('1.0'))->isNegative()
+    );
+
+    $this->assertSame(
+      false,
+      (new Decimal('0'))->isNegative()
+    );
+  }
+
+  /**
+   *
+   */
+  public function testCanDetectNegativeDecimals () {
+
+    $this->assertSame(
+      true,
+      (new Decimal('-1.23'))->isNegative(),
+      '-1.23'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('-1.2'))->isNegative(),
+      '-1.2'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('-1'))->isNegative(),
+      '-1'
+    );
+
+    $this->assertSame(
+      true,
+      (new Decimal('-1.0'))->isNegative(),
+      '-1.0'
+    );
+
+  }
+
+  /**
+   *
+   */
   public function testCanSerializeJSON () {
 
     $this->assertSame(
@@ -130,11 +260,602 @@ final class DecimalTest extends TestCase {
   /**
    *
    */
+  public function testCanTestGreaterThan () {
+
+    $this->assertSame(
+      true,
+      Decimal::isGreater( '124.56', '10.5'),
+      '124.56 > 10.5'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreater( '10.5', '1'),
+      '10.5 > 1'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreater( '10', '-10.5'),
+      '10 > -10.5'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreater( '10', '10'),
+      '10 > 10'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreater(  '10.5', '124.56'),
+      '10.5 > 124.56'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreater( '1', '10.5'),
+      '1 > 10.5'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreater( '-10.5', '10'),
+      '-10.5 > 10'
+    );
+
+  }
+  /**
+   *
+   */
+  public function testCanTestGreaterOrEqual () {
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '124.56', '10.5'),
+      '124.56 >= 10.5'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '10.5', '1'),
+      '10.5 >= 1'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '10', '-10.5'),
+      '10 >= -10.5'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '10', '10'),
+      '10 >= 10'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '-10', '-10'),
+      '-10 >= -10'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '0', '0.00'),
+      '0 >= 0.00'
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isGreaterOrEqual( '0.000100', '0.000100'),
+      '0.000100 >= 0.000100'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreaterOrEqual(  '10.5', '124.56'),
+      '10.5 >= 124.56'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreaterOrEqual( '1', '10.5'),
+      '1 >= 10.5'
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isGreaterOrEqual( '-10.5', '10'),
+      '-10.5 >= 10'
+    );
+
+  }
+
+  /**
+   *
+   */
+  public function testCanTestLowerThan () {
+
+    $this->assertSame(
+      true,
+      Decimal::isLower(  '10.5', '124.56')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLower( '1', '10.5')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLower( '-10.5', '10')
+    );
+
+
+    $this->assertSame(
+      false,
+      Decimal::isLower(   '124.56', '10.5')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLower(  '10.5', '1')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLower( '10', '-10.5')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLower( '10', '0')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLower( '-10', '0')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLower( '-10', '-10')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLower( '0', '0')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLower( '12', '12')
+    );
+
+  }
+
+  /**
+   *
+   */
+  public function testCanTestLowerOrEqual () {
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual(  '10.5', '124.56')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual( '1', '10.5')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual( '-10.5', '10')
+    );
+
+
+    $this->assertSame(
+      false,
+      Decimal::isLowerOrEqual(   '124.56', '10.5')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLowerOrEqual(  '10.5', '1')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLowerOrEqual( '10', '-10.5')
+    );
+
+    $this->assertSame(
+      false,
+      Decimal::isLowerOrEqual( '10', '0')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual( '-10', '0')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual( '-10', '-10')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual( '0', '0')
+    );
+
+    $this->assertSame(
+      true,
+      Decimal::isLowerOrEqual( '12', '12')
+    );
+
+  }
+
+  /**
+   *
+   */
   public function testCanTestEquality () {
 
     $this->assertSame(
       true,
       Decimal::isEqual( '124.56', '124.56')
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanFloorDecimals () {
+
+    $this->assertSame(
+      '124',
+      '' . Decimal::floor( new Decimal('124.56'))
+    );
+
+    $this->assertSame(
+      '124',
+      '' . Decimal::floor( new Decimal('124.1'))
+    );
+
+    $this->assertSame(
+      '14',
+      '' . Decimal::floor( new Decimal('14.00'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::floor( new Decimal('4.9'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::floor( new Decimal('4.0'))
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanFloorNegativeDecimals () {
+
+    $this->assertSame(
+      '-125',
+      '' . Decimal::floor( new Decimal('-124.56'))
+    );
+
+    $this->assertSame(
+      '-125',
+      '' . Decimal::floor( new Decimal('-124.1'))
+    );
+
+    $this->assertSame(
+      '-14',
+      '' . Decimal::floor( new Decimal('-14.00'))
+    );
+
+    $this->assertSame(
+      '-5',
+      '' . Decimal::floor( new Decimal('-4.9'))
+    );
+
+    $this->assertSame(
+      '-4',
+      '' . Decimal::floor( new Decimal('-4.0'))
+    );
+
+  }
+
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanCeilDecimals () {
+
+    $this->assertSame(
+      '125',
+      '' . Decimal::ceil( new Decimal('124.56'))
+    );
+
+    $this->assertSame(
+      '125',
+      '' . Decimal::ceil( new Decimal('124.1'))
+    );
+
+    $this->assertSame(
+      '14',
+      '' . Decimal::ceil( new Decimal('14.00'))
+    );
+
+    $this->assertSame(
+      '5',
+      '' . Decimal::ceil( new Decimal('4.9'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::ceil( new Decimal('4.0'))
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanCeilNegativeDecimals () {
+
+    $this->assertSame(
+      '-124',
+      '' . Decimal::ceil( new Decimal('-124.56'))
+    );
+
+    $this->assertSame(
+      '-124',
+      '' . Decimal::ceil( new Decimal('-124.1'))
+    );
+
+    $this->assertSame(
+      '-14',
+      '' . Decimal::ceil( new Decimal('-14.00'))
+    );
+
+    $this->assertSame(
+      '-4',
+      '' . Decimal::ceil( new Decimal('-4.9'))
+    );
+
+    $this->assertSame(
+      '-4',
+      '' . Decimal::ceil( new Decimal('-4.0'))
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanRoundDecimals () {
+
+    $this->assertSame(
+      '125',
+      '' . Decimal::round( new Decimal('124.56'))
+    );
+
+    $this->assertSame(
+      '124',
+      '' . Decimal::round( new Decimal('124.1'))
+    );
+
+    $this->assertSame(
+      '14',
+      '' . Decimal::round( new Decimal('14.00'))
+    );
+
+    $this->assertSame(
+      '5',
+      '' . Decimal::round( new Decimal('4.9'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::round( new Decimal('4.0'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::round( new Decimal('4.1'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::round( new Decimal('4.01'))
+    );
+
+    $this->assertSame(
+      '4',
+      '' . Decimal::round( new Decimal('4.49999'))
+    );
+
+    $this->assertSame(
+      '5',
+      '' . Decimal::round( new Decimal('4.5'))
+    );
+
+    $this->assertSame(
+      '5',
+      '' . Decimal::round( new Decimal('4.5001'))
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanRoundNegativeDecimals () {
+
+    $this->assertSame(
+      '-125',
+      '' . Decimal::round( new Decimal('-124.56'))
+    );
+
+    $this->assertSame(
+      '-124',
+      '' . Decimal::round( new Decimal('-124.1'))
+    );
+
+    $this->assertSame(
+      '-14',
+      '' . Decimal::round( new Decimal('-14.00'))
+    );
+
+    $this->assertSame(
+      '-5',
+      '' . Decimal::round( new Decimal('-4.9'))
+    );
+
+    $this->assertSame(
+      '-5',
+      '' . Decimal::round( new Decimal('-4.99999999999999999999999999999999999999999'))
+    );
+
+    $this->assertSame(
+      '-4',
+      '' . Decimal::round( new Decimal('-4.499999999999999'))
+    );
+
+    $this->assertSame(
+      '-4',
+      '' . Decimal::round( new Decimal('-4.499999999999999999999999999999999'))
+    );
+
+    $this->assertSame(
+      '-5',
+      '' . Decimal::round( new Decimal('-5.000000000000000000000000000000001'))
+    );
+
+    $this->assertSame(
+      '-4',
+      '' . Decimal::round( new Decimal('-4.0'))
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanAbsNegativeDecimals () {
+
+    $this->assertSame(
+      '124.56',
+      '' . Decimal::abs( new Decimal('-124.56'))
+    );
+
+    $this->assertSame(
+      '124.1',
+      '' . Decimal::abs( new Decimal('-124.1'))
+    );
+
+    $this->assertSame(
+      '14.00',
+      '' . Decimal::abs( new Decimal('-14.00'))
+    );
+
+    $this->assertSame(
+      '4.9',
+      '' . Decimal::abs( new Decimal('-4.9'))
+    );
+
+    $this->assertSame(
+      '4.99999999999999999999999999999999999999999',
+      '' . Decimal::abs( new Decimal('-4.99999999999999999999999999999999999999999'))
+    );
+
+    $this->assertSame(
+      '4.499999999999999',
+      '' . Decimal::abs( new Decimal('-4.499999999999999'))
+    );
+
+    $this->assertSame(
+      '4.499999999999999999999999999999999',
+      '' . Decimal::abs( new Decimal('-4.499999999999999999999999999999999'))
+    );
+
+    $this->assertSame(
+      '5.000000000000000000000000000000001',
+      '' . Decimal::abs( new Decimal('-5.000000000000000000000000000000001'))
+    );
+
+    $this->assertSame(
+      '4.0',
+      '' . Decimal::abs( new Decimal('-4.0'))
+    );
+
+  }
+
+  /**
+   *
+   * @noinspection PhpUnhandledExceptionInspection
+   */
+  public function testCanAbsPositiveDecimals () {
+
+    $this->assertSame(
+      '124.56',
+      '' . Decimal::abs( new Decimal('124.56'))
+    );
+
+    $this->assertSame(
+      '124.1',
+      '' . Decimal::abs( new Decimal('124.1'))
+    );
+
+    $this->assertSame(
+      '14.00',
+      '' . Decimal::abs( new Decimal('14.00'))
+    );
+
+    $this->assertSame(
+      '4.9',
+      '' . Decimal::abs( new Decimal('4.9'))
+    );
+
+    $this->assertSame(
+      '4.99999999999999999999999999999999999999999',
+      '' . Decimal::abs( new Decimal('4.99999999999999999999999999999999999999999'))
+    );
+
+    $this->assertSame(
+      '4.499999999999999',
+      '' . Decimal::abs( new Decimal('4.499999999999999'))
+    );
+
+    $this->assertSame(
+      '4.499999999999999999999999999999999',
+      '' . Decimal::abs( new Decimal('4.499999999999999999999999999999999'))
+    );
+
+    $this->assertSame(
+      '5.000000000000000000000000000000001',
+      '' . Decimal::abs( new Decimal('5.000000000000000000000000000000001'))
+    );
+
+    $this->assertSame(
+      '4.0',
+      '' . Decimal::abs( new Decimal('4.0'))
     );
 
   }
